e939ebf589243a54fd3eb32333dc9e0d0d13658a
Matt Hale <mlhale@unomaha.edu>
2017-05-26 01:41:38 -0500

    integrating with heatmap.js api

--- addon/components/ember-heatmap.js
+++ addon/components/ember-heatmap.js
@@ -3,9 +3,19 @@ import layout from '../templates/components/ember-heatmap';
 
 export default Ember.Component.extend({
   layout,
-  testvar: 1,
+  map: null,
   setupHeatmap: Ember.on('didInsertElement', function() {
+    //initiate heatmapjs root object by appending to this component
 
+    var config = {
+      container: Ember.$('#'+this.get('elementId')).get(0),
+      radius: 10,
+      maxOpacity: .5,
+      minOpacity: 0,
+      blur: .75
+    };
+    var map = h337.create(config);
+    this.set('map', map);
   }),
 
   teardownHeatmap: Ember.on('willDestroyElement', function() {
