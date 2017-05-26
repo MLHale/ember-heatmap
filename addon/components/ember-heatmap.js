fe83162b68a317e93fc89a2bdbfbc7254f7644cc
Matt Hale <mlhale@unomaha.edu>
2017-05-26 12:12:26 -0500

    including full config options

--- addon/components/ember-heatmap.js
+++ addon/components/ember-heatmap.js
@@ -4,21 +4,28 @@ import layout from '../templates/components/ember-heatmap';
 export default Ember.Component.extend({
   layout,
   map: null,
+
+  //Heatmap js config object
+  config: {
+    radius: 10,
+    maxOpacity: .5,
+    minOpacity: 0,
+    blur: .75
+  },
+
+  //Data to be placed in the heatmap
   data: [],
   min: 0,
   max: 100,
+
+  //set after heatmap is initialized
   hasRendered: false,
   classNames: ['heatmap-wrapper',],
   setupHeatmap: Ember.on('didInsertElement', function() {
     //initiate heatmapjs root object by appending to this component
 
-    var config = {
-      container: Ember.$('#'+this.get('elementId')).get(0),
-      radius: 10,
-      maxOpacity: .5,
-      minOpacity: 0,
-      blur: .75
-    };
+    var config = this.get('config');
+    config.container = Ember.$('#'+this.get('elementId')).get(0);
     var map = h337.create(config);
     if(this.get('data')){
       map.setData({
