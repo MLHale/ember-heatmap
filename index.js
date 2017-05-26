4b4783564229e06ef3c3796598c8eff04e183e59
Matt Hale <mlhale@unomaha.edu>
2017-05-26 01:02:59 -0500

    basic setup

--- index.js
+++ index.js
@@ -2,5 +2,21 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-heatmap'
+  name: 'ember-heatmap',
+  options: {
+    nodeAssets: {
+      'heatmapjs' : {
+        vendor: {
+          destDir: 'heatmapjs',
+          include: ['heatmap.js']
+        }
+      }
+    }
+  },
+  included: function(parent) {
+    this.addonOptions = parent.options && parent.options.myAddon || {};
+    this.addonOptions.theme = this.addonOptions.theme || 'light';
+    this._super.included.apply(this, arguments);
+    this.import('vendor/heatmapjs/heatmap.js');
+  },
 };
